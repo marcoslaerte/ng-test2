8ec1e3e8c8e44da93dc31b97e8b89cc07383391d
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-04-15 19:51:46 -0300

    Acrescentando debounceTime para controlar muitos cliques do usuário em um botão

--- src/app/shared/photo-frame/photo-frame.component.ts
+++ src/app/shared/photo-frame/photo-frame.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-frame',
@@ -6,13 +8,29 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./photo-frame.component.scss']
 })
 
-export class PhotoFrameComponent {
+export class PhotoFrameComponent implements OnInit, OnDestroy {
+
   @Output() public liked: EventEmitter<void> = new EventEmitter();
   @Input() public description = '';
   @Input() public src = '';
   @Input() public likes = 0;
+  private debounceSubject: Subject<void> = new Subject();
+  private unsubscribe: Subject<void> = new Subject();
+
+  public ngOnInit(): void {
+    this.debounceSubject
+      .asObservable()
+      .pipe(debounceTime(500))
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(() => this.liked.emit());
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
 
   public like(): void {
-    this.liked.emit();
+    this.debounceSubject.next();
   }
 }
