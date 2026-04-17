c4d57a5649b438c70405a57dcb7fb638af704b3d
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-04-17 19:44:58 -0300

    Simulando um clique

--- src/app/shared/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/photo-frame/photo-frame.component.spec.ts
@@ -82,4 +82,17 @@ describe(PhotoFrameComponent.name, () => {
     expect(img.getAttribute('alt')).toBe(description);
   });
 
+  it(`(D) Should display number of likes when clicked`, done => {
+    fixture.detectChanges();
+    component.liked.subscribe(() => {
+      component.likes++;
+      fixture.detectChanges();
+      const counterEl: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
+      expect(counterEl.textContent.trim()).toBe('1');
+      done();
+    });
+    const likeWidgetContainerEl: HTMLElement = fixture.nativeElement.querySelector('.like-widget-container');
+    likeWidgetContainerEl.click();
+  });
+
 });
